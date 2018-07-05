0396b9fc428aa75937ac31c0793d933131649ea9
Léo José da Costa <leoeduar@gmail.com>
2018-07-05 12:24:50 -0300

    Eureka e config server

---
+++ my-eureka/src/main/java/br/com/lelo/springcloud/myeureka/MyEurekaApplication.java
@@ -0,0 +1,14 @@
+package br.com.lelo.springcloud.myeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MyEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyEurekaApplication.class, args);
+    }
+}
