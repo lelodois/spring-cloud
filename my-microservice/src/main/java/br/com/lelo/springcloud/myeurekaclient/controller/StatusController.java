b298bbbad4d95978596ab69bed303ee89c4ea132
Léo José da Costa <leoeduar@gmail.com>
2018-07-06 09:38:55 -0300

    Change port mymicroservice

---
+++ my-microservice/src/main/java/br/com/lelo/springcloud/myeurekaclient/controller/StatusController.java
@@ -0,0 +1,20 @@
+package br.com.lelo.springcloud.myeurekaclient.controller;
+
+import br.com.lelo.springcloud.myeurekaclient.model.Message;
+import br.com.lelo.springcloud.myeurekaclient.model.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+
+    @Autowired
+    private MessageService service;
+
+    @GetMapping("status")
+    public ResponseEntity<Message> message() {
+        return ResponseEntity.ok(service.getStartupMessage());
+    }
+}
